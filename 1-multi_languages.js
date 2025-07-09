8fa1dd37bdaeb67d29f11c73fdceb6d054ba1d76
maame-gigi <159448426+maame-gigi@users.noreply.github.com>
2025-07-09 19:40:38 +0000

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,8 @@
 let senOne = 'C is fun';
 let senTwo = 'Python is cool';
 let senThree = 'JavaScript is amazing';
-console.log(senOne, senTwo, senThree);
+
+console.log(senOne);
+console.log(senTwo);
+console.log(senThree);
+
