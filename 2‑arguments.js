91f43a7d52e985747002af2df3267009dcab03e7
maame-gigi <159448426+maame-gigi@users.noreply.github.com>
2025-07-13 08:41:54 +0000

    Delete 2‑arguments.js

--- 2‑arguments.js
+++
@@ -1,9 +0,0 @@
-const argsLength = process.argv.length - 2;
-
-if (argsLength === 0) {
-  console.log('No argument');
-} else if (argsLength === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
